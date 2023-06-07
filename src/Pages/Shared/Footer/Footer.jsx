0bfe394dfb2da45d08f03353bec79ca1ee42497a
Ornob121 <shadmantahmid121@gmail.com>
2023-06-07 10:47:02 +0600

    navBar done

---
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer>
+      <hr className="h-[2px] text-gray-200" />
+      <h2>THis is footer</h2>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,103 @@
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../../assets/images/logo.svg";
+
+const NavBar = () => {
+  const navItem = (
+    <>
+      <li className="text-xl">
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "text-blue-500 border-b-2 border-blue-500" : ""
+          }
+          to="/"
+        >
+          Home
+        </NavLink>
+      </li>
+      <li className="text-xl">
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "text-blue-500 border-b-2 border-blue-500" : ""
+          }
+          to="/instructors"
+        >
+          Instructors
+        </NavLink>
+      </li>
+      <li className="text-xl">
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "text-blue-500 border-b-2 border-blue-500" : ""
+          }
+          to="/classes"
+        >
+          Classes
+        </NavLink>
+      </li>
+      <li className="text-xl">
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "text-blue-500 border-b-2 border-blue-500" : ""
+          }
+          to="/dashboard"
+        >
+          Dashboard
+        </NavLink>
+      </li>
+    </>
+  );
+
+  const logItems = (
+    <>
+      <button className="text-xl btn btn-info">
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "text-blue-500 border-b-2 border-blue-500" : ""
+          }
+          to="/login"
+        >
+          Login
+        </NavLink>
+      </button>
+    </>
+  );
+  return (
+    <div className="navbar sticky top-0 px-8 bg-white py-5 items-center">
+      <div className="navbar-start  ">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navItem}
+          </ul>
+        </div>
+        <Link className="btn btn-ghost normal-case text-xl">
+          <img src={logo} alt="" />
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navItem}</ul>
+      </div>
+      <div className="navbar-end">{logItems}</div>
+    </div>
+  );
+};
+
+export default NavBar;
